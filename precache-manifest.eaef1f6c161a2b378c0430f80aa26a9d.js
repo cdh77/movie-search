b59e1e6e2043ac21c6599a798d584097462ac5e0
cdh77 <orbitjazzy61@gmail.com>
2020-08-31 14:47:57 +0000

    Updates

---
+++ precache-manifest.eaef1f6c161a2b378c0430f80aa26a9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59f13bce831728f39fedc817d4b29d52",
+    "url": "/movie-search/index.html"
+  },
+  {
+    "revision": "1093ffad97be0ac4f491",
+    "url": "/movie-search/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "56eabe896d0100c9b3cf",
+    "url": "/movie-search/static/css/main.4a79d925.chunk.css"
+  },
+  {
+    "revision": "1093ffad97be0ac4f491",
+    "url": "/movie-search/static/js/2.1565b0c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-search/static/js/2.1565b0c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56eabe896d0100c9b3cf",
+    "url": "/movie-search/static/js/main.71c0c43e.chunk.js"
+  },
+  {
+    "revision": "8f1314dfdc21c95c9344",
+    "url": "/movie-search/static/js/runtime-main.8345f5b5.js"
+  }
+]);
